51b1c25e61099db1a4b95ac5f8c1c9ea4c61eba2
pc <1804551529@qq.com>
2019-11-04 21:22:03 +0800

    jijiji

---
+++ src/component/dome/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+//函数式组件 无状态 有属性 有返回值
+//类组件 有状态 有属性 有返回值
+
+function moduleFun(props) {
+    return <div >
+        111 <
+        /div>
+}
+//类组件
+class moduleCla extends Component {
+    state = {
+
+        }
+        //渲染虚拟dom 
+    render() {
+        return <div >
+
+            <
+            /div>
+    }
+}
+
+extends
+default moduleFun
